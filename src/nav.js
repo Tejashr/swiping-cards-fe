7414820ba68472184564d38bff6eeb3c3d29e173
Tejashr <tejasteju11@gmail.com>
2021-06-05 22:09:02 +0530

    button

--- src/nav.js
+++ src/nav.js
@@ -12,8 +12,8 @@ function Nav() {
         <>
             <nav class="navbar navbar-expand-lg navbar-light bg-dark">
                 <a class="navbar-brand text-light"><FontAwesomeIcon icon={faSun}></FontAwesomeIcon> Swiping Cards</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon "></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">                      
